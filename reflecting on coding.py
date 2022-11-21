21b1acbce9c510501ec5aeaa99f25cbac3a9a5cd
Amanda Mendoza Gonzalez <103083666+amanda517@users.noreply.github.com>
2022-11-21 10:40:25 -0500

    Add files via upload

---
+++ reflecting on coding.py
@@ -0,0 +1,30 @@
+def _init_(self , max_speed, condtion, price):
+  self.max_speed = max_speed
+  self.condition = condition 
+  self.price = price
+
+  def repair(self):
+    self.condition = "repaired"
+
+    def _init_(self, max_speed, condition, price):
+      super.init(max_speed, condition, price):
+
+
+    def boost(self:
+      self.max_speed *=2
+
+    class SebulbasPod(Podracer):
+    def _init_(self, max_speed, condition, price):
+      super.init(max_speed, condition, price):
+
+    def flame_jet(self, other):
+      other.condition = "trashed"
+  
+
+  #how does this solution ensure data immutability?
+  #This solution has a combination of validations done with a blockchain hashing process making it immutable. 
+
+  #Is this solution a pure function?
+  #Its not a pure function because a pure funtion is free from side-effects and returns a same output. 
+
+  
